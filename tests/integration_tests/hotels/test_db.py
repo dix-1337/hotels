f0b95b886f546878577aa84c5aa177aa8958ad70
Daniil <anatolijfrolov14@gmail.com>
2026-08-26 16:26:54 +0300

    chore: add separate check mode

--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -4,7 +4,7 @@ from src.utils.db_manager import DBManager
 
 
 async def test_add_hotel():
-    hotel_data = HotelAdd(title="Hotel 5 stars", location="Сочи")
+    hotel_data = HotelAdd(title="Hotel 6 stars", location="Сочи")
 
     async with DBManager(session_factory=async_session_maker) as db:
         new_hotel_data = await db.hotels.add(hotel_data)
